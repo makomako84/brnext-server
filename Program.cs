b5fec2eaedca790a54dabedbc780397acaab25ee
hanzzbrr <artur.galimsky@gmail.com>
2023-03-16 21:23:46 +0600

    build host with CreateDefaultBuilder

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
@@ -21,15 +22,44 @@ using Microsoft.Extensions.Options;
     source: https://learn.microsoft.com/en-us/dotnet/core/extensions/generic-host
 */
 
-var builder = new HostApplicationBuilder(args);
-builder.Services.AddSingleton<IFooService, FooService>();
+// var builder = new HostApplicationBuilder(args);
+// builder.Services.AddSingleton<IFooService, FooService>();
 
-// test config works ...
-var testSection = builder.Configuration.GetSection("ApiSettings:TwitterApiKey");
-System.Console.WriteLine(testSection.Value);
+// // test config works ...
+// var testSection = builder.Configuration.GetSection("ApiSettings:TwitterApiKey");
+// System.Console.WriteLine(testSection.Value);
 
 
-using var host = builder.Build();
+// using var host = builder.Build();
 
-// Здесь Host встает на ожидание и принимает всякие-разные запросы
+// // Здесь Host встает на ожидание и принимает всякие-разные запросы
+// await host.RunAsync();
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging((context, logging) =>
+    {
+        logging.ClearProviders();
+
+        logging.AddConsole();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<IFooService, FooService>();
+
+    }).Build();
+
+// Test IConfiguration works
+IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+var testSection = config.GetSection("ApiSettings:TwitterApiKey");
+System.Console.WriteLine("Config value is: " + testSection.Value);
+
+
+// Test service works
+var run = host.Services.GetRequiredService<IFooService>();
+run.DoThing(4);
+
+
+
+
+// Run host here ...
 await host.RunAsync();
