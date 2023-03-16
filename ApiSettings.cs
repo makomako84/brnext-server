c339f6c2ae8bebc34e65ec333022d34ce10e22dd
hanzzbrr <artur.galimsky@gmail.com>
2023-03-16 17:22:13 +0600

    initial

---
+++ ApiSettings.cs
@@ -0,0 +1,6 @@
+ public class ApiSettings
+    {
+        public string TwitterApiKey { get; set; }
+        public string TwitterApiSecret { get; set; }
+        public string BearerToken { get; set; }
+    }
---
+++ Startup.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.Configuration;
+
+public class Startup
+    {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                      .SetBasePath(Directory.GetCurrentDirectory())
+                      .AddJsonFile("appsettings.json", optional: false);
+
+            IConfiguration config = builder.Build();
+
+            var TwitterApiKey = config.GetSection("ApiSettings:TwitterApiKey");
+            System.Console.WriteLine(TwitterApiKey.Value);
+
+        }
+
+        public ApiSettings ApiSettings { get; private set; }
+    }
